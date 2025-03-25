fa746f2c8aafad68ae2a65396866e77f42d55710
Nikita-Rasshivalov <nikita_rasshivalov@mail.ru>
2025-03-25 18:21:14 +0300

    toggleFullScreen
    toggleFullScreen

--- src/components/Player/Controls/FullScreenButton.jsx
+++ src/components/Player/Controls/FullScreenButton.jsx
@@ -1,33 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { Maximize } from "lucide-react";
 
 export const FullScreenButton = ({toggleFullScreen}) => {
-
-  const buttonRef = useRef(null); 
-
-  useEffect(() => {
-    const button = buttonRef.current;
-
-    const handleClick = () => {
-      toggleFullScreen(); 
-    };
-
-    if (button) {
-      button.addEventListener("click", handleClick);
-      button.addEventListener("onTouchStart", handleClick);
-    }
-    return () => {
-      if (button) {
-        button.removeEventListener("click", handleClick);
-        button.removeEventListener("onTouchStart", handleClick);
-      }
-    };
-  }, [toggleFullScreen]); 
-
-
   return (
     <>
-      <button  ref={buttonRef} className="control-button">
+      <button onClick={toggleFullScreen} className="control-button">
         <Maximize size={20} />
       </button>
     </>
