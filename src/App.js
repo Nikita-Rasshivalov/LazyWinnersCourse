7a23460a675c8a2b46ef9c91ab05af6bbae66b1f
Nikita-Rasshivalov <nikita_rasshivalov@mail.ru>
2025-03-27 21:58:52 +0300

    add ReactGA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Layout from "./components/Layout";
 import React, { useEffect, useState } from 'react';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GTAG);
 
 function App() {
   const [mounted, setMounted] = useState(false);
